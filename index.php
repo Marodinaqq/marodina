67090013dc6828102385c03adbee9d12b2966906
marodinaq <nikitosina20092020@gmail.com>
2025-03-30 18:36:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="icon" href="https://github.com/Marodinaqq/marodina/blob/main/icon.png" type="image/x-icon">
     <title>Мародина</title>
     <script type="text/javascript">
         function insertEmoji(emoji) { document.getElementById('message').value += emoji; }
